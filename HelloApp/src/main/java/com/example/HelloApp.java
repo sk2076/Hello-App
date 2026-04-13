018fcb7c2bcbcae1b9f4ef60208a54b4a3747bf8
Siva Sagar <sk2076@srmist.edu.in>
2026-04-13 14:57:09 +0530

    UC4 done

--- HelloApp/src/main/java/com/example/HelloApp.java
+++ HelloApp/src/main/java/com/example/HelloApp.java
@@ -1,10 +1,21 @@
 package com.example;
 public class HelloApp {
     public static void main(String[] args) {
-        String name = "World";
+        String defname = "World";
+
         if (args.length > 0) {
-            name = args[0];
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < args.length; i++) {
+                sb.append(args[i]);
+
+                if (i < args.length - 1) {
+                    sb.append(", ");
+                }
+            }
+            System.out.println("Hello " + sb + "!");
+        } else {
+            System.out.println("Hello " + defname + "!");
         }
-        System.out.println("Hello," + name + "!");
     }
 }
