4a4a67c2db4ff05ba7aced7adf9046c4b30a3a92
Siva Sagar <sk2076@srmist.edu.in>
2026-04-13 14:36:41 +0530

    UC2 done

--- HelloApp/src/main/java/com/example/HelloApp.java
+++ HelloApp/src/main/java/com/example/HelloApp.java
@@ -1,6 +1,7 @@
 package com.example;
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String name = args[0];
+        System.out.println("Hello," + name +"!");
     }
 }
